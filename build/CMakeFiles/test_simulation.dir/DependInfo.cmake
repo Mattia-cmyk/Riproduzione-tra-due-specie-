3783e9a8ba2e1c642bedea77fd1be92089ae011c
Matti <mattialgiuffrida@gmail.com>
2024-07-05 01:02:21 +0200

    aggiunto cmake con la build, cmake completo anche per test credo

---
+++ build/CMakeFiles/test_simulation.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mattia/progetto3/src/classe.cpp" "CMakeFiles/test_simulation.dir/src/classe.cpp.o" "gcc" "CMakeFiles/test_simulation.dir/src/classe.cpp.o.d"
+  "/home/mattia/progetto3/test/test_simulation.cpp" "CMakeFiles/test_simulation.dir/test/test_simulation.cpp.o" "gcc" "CMakeFiles/test_simulation.dir/test/test_simulation.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/test_simulation.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_simulation.dir/src/classe.cpp.o"
+  "CMakeFiles/test_simulation.dir/src/classe.cpp.o.d"
+  "CMakeFiles/test_simulation.dir/test/test_simulation.cpp.o"
+  "CMakeFiles/test_simulation.dir/test/test_simulation.cpp.o.d"
+  "test_simulation"
+  "test_simulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
